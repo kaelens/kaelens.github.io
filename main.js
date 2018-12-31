a9bc65e95b8811149e9b693ca1ea4b856095c32f
Kaelen Saythongkham <kaelensayth@gmail.com>
2018-12-31 15:21:16 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 <!--
-$(function() {  
-  $('.btn-6')
-    .on('mouseenter', function(e) {
-			var parentOffset = $(this).offset(),
-      		relX = e.pageX - parentOffset.left,
-      		relY = e.pageY - parentOffset.top;
-			$(this).find('span').css({top:relY, left:relX})
-    })
-    .on('mouseout', function(e) {
-			var parentOffset = $(this).offset(),
-      		relX = e.pageX - parentOffset.left,
-      		relY = e.pageY - parentOffset.top;
-    	$(this).find('span').css({top:relY, left:relX})
-    });
-  $('[href=#]').click(function(){return false});
+$(function() {
+  $(".btn-9")
+    .on("mouseenter", function(e) {
+    var parentOffset = $(this).offset(),
+        relX = e.pageX - parentOffset.left,
+        relY = e.pageY - parentOffset.top;
+    $(this)
+      .find("span")
+      .css({ top: relY, left: relX });
+  })
+    .on("mouseout", function(e) {
+    var parentOffset = $(this).offset(),
+        relX = e.pageX - parentOffset.left,
+        relY = e.pageY - parentOffset.top;
+    $(this)
+      .find("span")
+      .css({ top: relY, left: relX });
+  });
+  $("[href=#]").click(function() {
+    return false;
+  });
 });
 -->
 
