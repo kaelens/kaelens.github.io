fc58ce541777adf0d8216438ab231e64e3c622c2
Kaelen Saythongkham <kaelensayth@gmail.com>
2018-12-31 13:40:53 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+$(function() {  
+  $('.btn-6')
+    .on('mouseenter', function(e) {
+			var parentOffset = $(this).offset(),
+      		relX = e.pageX - parentOffset.left,
+      		relY = e.pageY - parentOffset.top;
+			$(this).find('span').css({top:relY, left:relX})
+    })
+    .on('mouseout', function(e) {
+			var parentOffset = $(this).offset(),
+      		relX = e.pageX - parentOffset.left,
+      		relY = e.pageY - parentOffset.top;
+    	$(this).find('span').css({top:relY, left:relX})
+    });
+  $('[href=#]').click(function(){return false});
+});
+
+
 //My failed attempt at something cool
 // Wrap every letter in a span
 //$('.ml1 .letters').each(function(){
