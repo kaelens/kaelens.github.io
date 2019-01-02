0edf8ec64e1c95aa66f38ca7f6452b865a8478c9
Kaelen Saythongkham <kaelensayth@gmail.com>
2019-01-02 13:03:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(function(){
 	   setInterval(function(){
-	      $(".container ul").animate({marginLeft:-750},1500,function(){
+	      $(".container ul").animate({marginLeft:-850},1500,function(){
 		 $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
 	      })
 	   }, 4000);
