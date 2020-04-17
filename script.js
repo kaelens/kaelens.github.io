ed7b162577037ed81590dc09b34694c8bba026d5
Kaelen Saythongkham <kaelensayth@gmail.com>
2020-04-17 16:49:00 -0500

    deleted unused code

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-$("#slideshow > .fade:gt(0)").hide();
 
-setInterval(function() { 
-  $('#slideshow > .fade:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(2000)
-    .end()
-    .appendTo('#slideshow');
-},  4000);
